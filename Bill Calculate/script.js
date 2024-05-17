40c2a86c869cd3ce388e15d755e504896123e5a4
AlokSingh100 <159884599+AlokSingh100@users.noreply.github.com>
2024-05-17 03:48:15 -0700

    Bill calculating System

---
+++ Bill Calculate/script.js
@@ -0,0 +1,24 @@
+
+function bill(){
+     product = document.getElementById("product").value;
+     price = document.getElementById("price").value;
+     quantity = document.getElementById("quantity").value;
+     option= document.getElementById("option").value;
+     pr = (price * quantity);
+   
+     total1=document.getElementById("total");
+    switch(option){
+        case "5": total1.innerHTML=pr * 5/100;
+        break;
+        case "12": total1.innerHTML=(pr * 12)/100;
+        break;
+        case "18": total1.innerHTML=(pr * 18)/100;
+        break;
+        case "28": total1.innerHTML=(pr * 28)/100;
+        break;
+        default:alert("Pleac select any 1 option");
+    }
+document.getElementById("pri").innerHTML=pr;
+    document.getElementById("pro1").innerHTML=product;
+    document.getElementById("gs").innerHTML=option;
+}
