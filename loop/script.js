e459ee176de4e7c817b2c9a07c4e033ef1cc58fa
AlokSingh100 <159884599+AlokSingh100@users.noreply.github.com>
2024-05-18 12:02:26 +0530

    loop in box

--- loop/script.js
+++ loop/script.js
@@ -1,8 +1,15 @@
-function print(){
+function printe(){
     let start=document.getElementById("start").value;
     let end=document.getElementById("end").value;
-let bos=document.getElementById("bos");
+    let bos=document.getElementById("bos");
 
-    for( let i=start;i<=end;i++)
-        bos.innerHTML +=<li>+i+</li>;
+    for( let i=start;i<=end;i++){
+
+        if (i%2==0){ 
+            bos.innerHTML +="<li>"+i+"</li>";
+        }
+        else{
+            bos.innerHTML +="<li style='background-color: aqua;'>"+i+"</li>";
+        }
+    }
 }
