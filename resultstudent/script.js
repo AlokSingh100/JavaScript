0ed924557828255cb11e9cf564df6e4a6f65ac11
AlokSingh100 <159884599+AlokSingh100@users.noreply.github.com>
2024-05-30 04:33:51 -0700

    student result in js

---
+++ resultstudent/script.js
@@ -0,0 +1,45 @@
+function Submit1(){
+   let snames=document.getElementById("snames").value;
+   let roll=document.getElementById("roll").value;
+   let clas=document.getElementById("clas").value;
+   let javaM=document.getElementById("javaM").value;
+   let cM=document.getElementById("cM").value;
+   let cplM=document.getElementById("cplM").value;
+   let pythonM=document.getElementById("pythonM").value;
+   let phpM=document.getElementById("phpM").value;
+
+   let total= +javaM + +cM + +cplM + +pythonM + +phpM;
+   let percentage= (total*100)/500;
+   if(javaM>=35 && cM>=35 && cplM>=30 && pythonM>=35 && phpM>=35)
+    {
+       if(percentage>=80 && percentage<=100){
+        document.getElementById("gread").innerHTML="A"
+       }
+       else if(percentage>=70 && percentage<=79){
+        document.getElementById("gread").innerHTML="B"
+       }
+       else if(percentage>=60 && percentage<=69){
+        document.getElementById("gread").innerHTML="C"
+       }
+       else if(percentage>=50 && percentage<=59){
+        document.getElementById("gread").innerHTML="D"
+       }
+        
+    }
+    else{
+        document.getElementById("gread").innerHTML="Fail"
+    }
+    document.getElementById("naame").innerHTML=snames;
+    document.getElementById("rno").innerHTML=roll;
+    document.getElementById("class").innerHTML=clas;
+    document.getElementById("java").innerHTML=javaM+"/100";
+    document.getElementById("c").innerHTML=cM+"/100";
+    document.getElementById("cpl").innerHTML=cplM+"/100";
+    document.getElementById("python").innerHTML=pythonM+"/100";
+    document.getElementById("php").innerHTML=phpM+"/100";
+    document.getElementById("total1").innerHTML=total+"/500";
+    document.getElementById("per").innerHTML=percentage;
+
+
+
+}
